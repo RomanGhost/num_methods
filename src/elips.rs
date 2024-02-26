e1e192cc0d50010c08882d86321ef92d442d7b9c
Ghost <rildiarov@gmail.com>
2024-02-26 23:12:21 +0400

    Lab 1 complete accounting

--- src/elips.rs
+++ src/elips.rs
@@ -1,14 +1,17 @@
 pub fn elipsoid64(eps:f64) ->f64{
+    let mut count=0;
     let mut new_eps:f64 = eps;
     let mut new_eps1:f64 = 0.0;
 
     loop{
+        count+=1;
         new_eps /= 2.0;
         new_eps1 = new_eps + 1.0;
         if new_eps1 <= 1.0{
             break;
         }
     }
+    println!("{}", count);
     new_eps
 }
 
